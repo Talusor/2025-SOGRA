4b47e5207e82a3a7a0e9820e125144a02e52e8e6
Talusor <lth030326@naver.com>
2025-11-08 20:44:41 +0900

    NaverMAP SDK 추가

--- app/src/main/java/com/example/daejeongyu/MainActivity.kt
+++ app/src/main/java/com/example/daejeongyu/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.daejeongyu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.NaverMapSdk
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NcpKeyClient(BuildConfig.MAP_API_KEY);
     }
 }
